1d9d12d24cea9436012f87cb281ec80c70f2416f
Oluwatos94 <akinbowat@gmail.com>
2024-05-21 06:34:43 +0100

    fix: made adjustment

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [color, setColor] = useState('olive')
+  
   return (
     <div className='w-full h-screen duration-200 ' style={{backgroundColor: color}}>
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-        <button
+          <button
           onClick={() => setColor('red')}
           className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
           style={{backgroundColor: 'red'}}
